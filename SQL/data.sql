9734314caa357d9ff927989876022ec8f5137fab
ceomarek <mrk.kolodziejczyk@gmail.com>
2015-10-08 15:54:26 +0200

    PAY-17 stworzenie struktury danych zobowiazan i danych testowych

---
+++ SQL/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `contract` (`id`, `Signature`) VALUES
+(0, 'SN/234/234'),
+(1, 'SN/234/2356'),
+(2, 'SN/234/98564');
---
+++ SQL/structure.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `contract` (
+  `id` int(11) NOT NULL,
+  `Signature` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin2;
+
+ALTER TABLE `contract`
+  ADD PRIMARY KEY (`id`);
