c2c46ee1a518ea186a62c40fcdbfb1daca3c732d
Paweł Zawodny <pawel.zawodny@gmail.com>
2015-10-14 14:15:08 +0200

    Nowy ekran logowania, drobne porządki w strukturze plików

--- index.php
+++ index.php
@@ -9,13 +9,8 @@ include 'session_check_header.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <link rel="icon" href="../../favicon.ico">
     <title>Pied Piper payments</title>
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="dashboard.css" rel="stylesheet">
-    <script src="../../assets/js/ie8-responsive-file-warning.js"></script>
-    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <link href="css/dashboard.css" rel="stylesheet">
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   </head>
 
