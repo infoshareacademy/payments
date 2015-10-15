ca019555153182ef19824c714d1769b18869c5fc
ceomarek <mrk.kolodziejczyk@gmail.com>
2015-10-15 08:55:17 +0200

    PAY-2 dodanie zapisu do bazy danych - polaczenie do bazy jest, ale nie zapisuje rekordu - poprawiona walidacja dat

--- Model/InvoiceClass.php
+++ Model/InvoiceClass.php
@@ -9,7 +9,7 @@
 class InvoiceClass
 {
 
-    protected $id;
+//    protected $id;
     protected $signature;
     protected $amount;
     protected $issue_date;
@@ -17,39 +17,43 @@ class InvoiceClass
     protected $payment_date;
 
     const MAX_LENGHT = 70;
+    const SAVE_OK = 1;
+    const SAVE_ERROR_DB = 2;
+    const SAVE_ERROR_DUPLICATE_SIG = 3;
 
-    private $pdo;
 
-    public function __construct($id = null)
-    {
-        $this->pdo = new PDO('mysql:dbname=infoshareaca_7;host=test.payments.infoshareaca.nazwa.pl', 'infoshareaca_7', 'F0r3v3r!');
-
-        if ($id) {
-            $stmt = $this->pdo->query("select * from invoices where id=".(int)$id);
-            if ($stmt->rowCount()>0) {
-                $result = $stmt->fetch(PDO::FETCH_ASSOC);
-                $this->id = $result['id'];
-                $this->signature = $result['signature'];
-                $this->amount = $result['amount'];
-                $this->issue_date = $result['issue_date'];
-                $this->maturity_date = $result['maturity_date'];
-                $this->payment_date = $result['payment_date'];
-            }
-            else {
-                throw new Exception('Brak takiego ID='.$id);
-            }
-        }
-
-    }
+//    private $pdo;
+//
+//    public function __construct($id = null)
+//    {
+//        $this->pdo = new PDO('mysql:dbname=infoshareaca_7;host=test.payments.infoshareaca.nazwa.pl', 'infoshareaca_7', 'F0r3v3r!');
+//
+//        if ($id) {
+//            $stmt = $this->pdo->query("select * from invoices where id=".(int)$id);
+//            if ($stmt->rowCount()>0) {
+//                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+//                $this->id = $result['id'];
+//                $this->signature = $result['signature'];
+//                $this->amount = $result['amount'];
+//                $this->issue_date = $result['issue_date'];
+//                $this->maturity_date = $result['maturity_date'];
+//                $this->payment_date = $result['payment_date'];
+//            }
+//            else {
+//                throw new Exception('Brak takiego ID='.$id);
+//            }
+//        }
+//
+//    }
 
     public function __set($param_name, $param_value) {
         switch ($param_name) {
-            case 'id':
-                if (!$param_value || !(int)$param_value)
-                    $this->id = null;
-                else
-                    $this->id = (int)$param_value;
-                break;
+//            case 'id':
+//                if (!$param_value || !(int)$param_value)
+//                    $this->id = null;
+//                else
+//                    $this->id = (int)$param_value;
+//                break;
             case 'signature':
                 if (!$param_value || strlen($param_value)>self::MAX_LENGHT)
                     $this->signature = null;
@@ -63,19 +67,19 @@ class InvoiceClass
                     $this->amount = (int)$param_value;
                 break;
             case 'issue_date':
-                if (!$param_value || !preg_match('/^[\d]{2}\.[\d]{2}\.[\d]{4}$/sx', $param_value))
+                if (!$param_value || !preg_match('/^[\d]{4}\-[\d]{2}\-[\d]{2}$/', $param_value))
                     $this->issue_date = null;
                 else
                 $this->issue_date = $param_value;
                 break;
             case 'maturity_date':
-                if (!$param_value || !preg_match('/^[\d]{2}\.[\d]{2}\.[\d]{4}$/sx', $param_value))
+                if (!$param_value || !preg_match('/^[\d]{4}\-[\d]{2}\-[\d]{2}$/sx', $param_value))
                     $this->maturity_date = null;
                 else
                 $this->maturity_date = $param_value;
                 break;
             case 'payment_date':
-                if (!$param_value || !preg_match('/^[\d]{2}\.[\d]{2}\.[\d]{4}$/sx', $param_value))
+                if (!preg_match('/^[\d]{4}\-[\d]{2}\-[\d]{2}$/sx', $param_value))
                     $this->payment_date = null;
                 else
                 $this->payment_date = $param_value;
@@ -91,6 +95,45 @@ class InvoiceClass
         return $this->$param_name;
     }
 
+    public function save_to_db() {
+        $pdo = new PDO ('mysql:dbname=infoshareaca_7;host=test.payments.infoshareaca.nazwa.pl', 'infoshareaca_7', 'F0r3v3r!');
+
+ // sprawdzam połączenie do bazy danych
+
+        if (!$pdo) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        echo "Podłączono do bazy danych"."<br>";
+
+// połączenie z bazą i sprawdzenie czy numer faktury nie dubluje się
+
+        $stmt = $pdo->query("select * from invoices WHERE Signature='".$this->signature."'");
+        if ($stmt->rowCount()>0)
+            return self::SAVE_ERROR_DUPLICATE_SIG;
+
+ // zapisanie do bazy danych
+
+        $stmt = $pdo->prepare("INSERT INTO invoices VALUES (NULL, :signature, :amount, :issue_date, :maturity_date, :payment_date)");
+        $upload = $stmt->execute(
+            array(
+                ':signature'=>$this->signature,
+                ':amount'=>$this->amount,
+                ':issue_date'=>$this->issue_date,
+                ':maturity_date'=>$this->maturity_date,
+                ':payment_date'=>$this->payment_date,
+
+            )
+        );
+
+// wyświetlenie komunikatu o stanie zapisu
+
+        if ($upload)
+            return self::SAVE_OK;
+        else
+            return self::SAVE_ERROR_DB;
+
+    }
+
 
 
 }
@@ -99,7 +142,7 @@ $error = array();
 if (count($_POST)) {
     $invoice = new InvoiceClass();
 
-    $invoice->id = @$_POST['id'];
+//    $invoice->id = @$_POST['id'];
 
     $invoice->signature = @$_POST['signature'];
     if (!$invoice->signature) // if null
@@ -122,17 +165,20 @@ if (count($_POST)) {
         $error['payment_date'] = 'Podaj poprawną datę lub skorzystaj z kalendarza';
 
 
-//    if (!count($error)) {
-//        $status = $invoice->save();
-//        if ($status==InvoiceClass::SAVE_STATUS_OK)
-//            $success = 'Brawo! Dodales nowy rekord do bazy';
-//        else if ($status==InvoiceClass::SAVE_STATUS_DUPLICATE_PHONE) {
-//            $error['phone'] = 'Telefon juz istnieje w bazie. Podaj inny.';
-//        }
-//        else {
-//            $error['general'] = 'Blad zapisu do bazy danych.';
-//        }
-//    }
+    if (!count($error)) {
+        $upload = $invoice->save_to_db();
+        if ($upload==InvoiceClass::SAVE_OK)
+            $success = 'Brawo! Dodales nowy rekord do bazy';
+        else if ($upload==InvoiceClass::SAVE_ERROR_DUPLICATE_SIG) {
+            $error['sig'] = 'Faktura o tym numerze już istnieje.';
+        }
+        else {
+            $error['general'] = 'Blad zapisu do bazy danych.';
+        }
+    }
+
+    echo '<pre>';
+    print_r($invoice);
 }
 
 ?>
@@ -146,18 +192,21 @@ if (count($_POST)) {
 <body>
 
 <?php
+
+    if (@$success)
+        echo '<div style="color:#22aa22; font-weight:bold;">'.$success.'</div><br/>';
+    if (@$error)
+        echo '<div style="color:#f00; font-weight:bold;">'.$error['general'].'</div><br/>';
+
 echo '<form action="?" method="post">';
 echo 'Numer faktury: <input name="signature" value="'.@$invoice->signature.'"/><br><div style="color:#f00;">'.@$error['signature'].'</div>';
 echo 'Kwota: <input name="amount" value="'.@$invoice->amount.'" /><br><div style="color:#f00;">'.@$error['amount'].'</div>';
 echo 'Data wystawienia: <input name="issue_date" type="date" value="'.@$invoice->issue_date.'" /><br><div style="color:#f00;">'.@$error['issue_date'].'</div>';
 echo 'Data płatności: <input name="maturity_date" type="date" value="'.@$invoice->maturity_date.'" /><br><div style="color:#f00;">'.@$error['maturity_date'].'</div>';
-echo 'Data opłacenia: <input name="payment_date" type="date" value="'.@$invoice->payment_date.'" /><br><div style="color:#f00;">'.@$error['payment_date'].'</div>';
+echo 'Data opłacenia: <input name="payment_date" type="date" value="'.@$invoice->payment_date.'" /><br>';
 
 echo '<button id="btn_send">ZAPISZ</button>';
 echo '</form>';
-
-//echo '<pre>';
-//var_dump($invoice);
 
 ?>
 
