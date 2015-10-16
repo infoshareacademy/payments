b559adb315689d3d24ece01d86864feca2e7fb1c
pawel <pawel@gmailcom>
2015-10-16 15:46:39 +0200

    Comments in mail_notifications

--- mail_notifications.php
+++ mail_notifications.php
@@ -6,6 +6,7 @@ require 'sqli_connection_header.php';
 $sql = 'SELECT id, Signature, Amount, Issue_date, Maturity_date FROM invoices WHERE Maturity_date = CURRENT_DATE ';
 $sqlQueryContent = mysqli_query($conn, $sql);
 
+//Creating content for PhpMailer Body. First make HTML table, then fill the table in while loop with data downloaded from database.
 $MailContent = '
 
     <table>
@@ -35,6 +36,8 @@ while ($result = $sqlQueryContent->fetch_assoc()) {
         . "</tr>";
 }
 
+// End of Creating PhpMailer Body.
+
 $mail = new PHPMailer();
 $mail->IsHTML(true);
 $mail->Subject = 'PAYMENTS - przypomnienia o płatnościach na dzień:' . ' ' . date('d.m.Y');
@@ -42,6 +45,7 @@ $mail->AddAddress('pawel.zawodny@gmail.com');
 $mail->Body = " <b>Twoje dzisiejsze faktury do opłacenia:</b> \n";
 $mail->Body .= $MailContent;
 
+//if there are invoices in database that need to be payed today, send notification email.
 if ($sqlQueryContent->num_rows > 0) {
 
     $mail->Send();
