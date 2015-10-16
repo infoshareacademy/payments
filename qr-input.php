6bc223388d374b83013f128e0cf69d4088a53af5
jakubmatyka <jaqbec@gmail.com>
2015-10-16 14:59:53 +0200

    PAY-6 - skrypt działa, w wyniku daje tablice asocjacyjną

--- qr-input.php
+++ qr-input.php
@@ -5,28 +5,50 @@
  * Date: 08.10.15
  * Time: 16:18
  */
+ //
+ $tempFile =  $_FILES['qrFile']['tmp_name'];
 
-//print_r($_FILES);
- //move_uploaded_file($_FILES['qrFile']['tmp_name'], 'test.png');
+ $imginfo_array = getimagesize($tempFile);
 
- require('lib/QrReader.php');
- $qrcode = new QrReader($_FILES['qrFile']['tmp_name']);
+ if ($imginfo_array !== false) {
+  $mime_type = $imginfo_array['mime'];
+  switch($mime_type) {
 
- $text = $qrcode->text();
-//
-//print_r($text);
-// exit;
-// list($nip,$language,$accountNumber,$amount,$operator,$invoiceNumber)
+   case "image/jpeg":
+    require('lib/QrReader.php');
+    $qrcode = new QrReader($_FILES['qrFile']['tmp_name']);
+    $text = $qrcode->text();
+    $qrcode_array = explode('|', $text.'<br/>');
+    list($id,$signature,$amount,$issue_date,$maturity_date,$payment_date) = explode('|', $text.'<br/>');
+    echo $id.'<br/>';
+    echo $signature.'<br/>';
+    echo $amount.'<br/>';
+    echo $issue_date.'<br/>';
+    echo $maturity_date.'<br/>';
+    echo $payment_date.'<br/>';
+    break;
 
- $qrcode_array = explode('|', $text.'<br/>');
- print_r($qrcode_array);
+   case "image/png":
+    require('lib/QrReader.php');
+    $qrcode = new QrReader($_FILES['qrFile']['tmp_name']);
+    $text = $qrcode->text();
+    $qrcode_array = explode('|', $text.'<br/>');
+    list($id,$signature,$amount,$issue_date,$maturity_date,$payment_date) = explode('|', $text.'<br/>');
+    echo $id.'<br/>';
+    echo $signature.'<br/>';
+    echo $amount.'<br/>';
+    echo $issue_date.'<br/>';
+    echo $maturity_date.'<br/>';
+    echo $payment_date.'<br/>';
+    break;
+
+  }
+ }
+ else {
+  echo "This is not a valid image file";
+ }
 
 
- echo $nip.'<br/>';
- echo $language.'<br/>';
- echo $accountNumber.'<br/>';
- echo $amount.'<br/>';
- echo $operator.'<br/>';
- echo $invoiceNumber.'<br/>';
+
 
 ?>
