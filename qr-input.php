1bd9566fbdb14b154640340a9688037abe247d76
jakubmatyka <jaqbec@gmail.com>
2015-10-16 17:22:52 +0200

    PAY-6 - post danych z formularza

--- qr-input.php
+++ qr-input.php
@@ -18,18 +18,16 @@
     require('lib/QrReader.php');
     $qrcode = new QrReader($_FILES['qrFile']['tmp_name']);
     $text = $qrcode->text();
-    $qrcode_array = explode('|', $text.'<br/>');
-    list($id,$signature,$amount,$issue_date,$maturity_date,$payment_date) = explode('|', $text.'<br/>');
+    $qrcode_array = explode('|', $text);
+    list($id,$signature,$amount,$issue_date,$maturity_date,$payment_date) = explode(' | ', $text);
     break;
 
    case "image/png":
     require('lib/QrReader.php');
     $qrcode = new QrReader($_FILES['qrFile']['tmp_name']);
     $text = $qrcode->text();
-    $qrcode_array = explode('|', $text.'<br/>');
-    list($id,$signature,$amount,$issue_date,$maturity_date,$payment_date) = explode('|', $text.'<br/>');
-    print_r($maturity_date);
-
+    $qrcode_array = explode('|', $text);
+    list($id,$signature,$amount,$issue_date,$maturity_date,$payment_date) = explode(' | ', $text);
     break;
 //    id=76&signature_id=6&signature=PGNIG%2F9&amount=41.00&issue_date=2015-11-01&maturity_date=2015-11-22&payment_date=2015-10-23
 
@@ -44,15 +42,14 @@
 
  <html>
  <body>
- <form method="post" action="invoices.php" id="qr-form">
-  <input type="hidden" name="signature_id" value="<?php echo $signature ?>">
-  <input type="hidden" name="amount_id" value="<?php echo $amount ?>">
-  <input type="hidden" name="issue_date_id" value="<?php echo $issue_date ?>">
-  <input type="hidden" name="maturity_date_id" value="<?php echo $maturity_date ?>">
-  <input type="hidden" name="payment_date_id" value="<?php echo $payment_date ?>">
+ <form method="post" action="index.php" id="qr-form">
+  <input type="hidden" name="signature" value="<?php echo $signature ?>">
+  <input type="hidden" name="amount" value="<?php echo $amount ?>">
+  <input type="hidden" name="issue_date" value="<?php echo $issue_date ?>">
+  <input type="hidden" name="maturity_date" value="<?php echo $maturity_date ?>">
+  <input type="hidden" name="payment_date" value="<?php echo $payment_date ?>">
+  <button type="submit"> Zapisz</button>
  </form>
- <script language="JavaScript">
-  document.getElementById("qr-form").submit();
- </script>
+
  </body>
  </html>
