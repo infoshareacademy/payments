63a3182319750c6d9724acf0620cab2469123bd1
tomek <tmietkiewicz@gmail.com>
2015-10-16 11:48:18 +0200

    PAY-2 Dodanie obiektu contract i prostego formularza na dodawanie i edycję Umowy do bazy. Bez funkcjonalności uploadu. Dodanie testowych plików z umowami

--- PAY7.php
+++ PAY7.php
@@ -5,14 +5,8 @@
  * Date: 09.10.15
  * Time: 12:22
  */
+
 include 'config.php';
-
-
-
-
-
-
-
 function upload_file($file_field, $force_type='') {
     $user_filename = $_POST['contractId'];
     $extension = substr($user_filename, strrpos($user_filename, '.')+1, 5);
@@ -24,26 +18,21 @@ function upload_file($file_field, $force_type='') {
 
     $status = move_uploaded_file($file_field['tmp_name'], ContractsUploadDirectory . $server_filename);
 
-    $mime = mime_content_type(ContractsUploadDirectory . $server_filename);
-    if ($force_type && $force_type!=$mime) {
-        unlink(ContractsUploadDirectory . $server_filename);
-        return 'BLEDNY TYP PLIKU, WYMAGAMY PDF'.$force_type;
-    }
-
     if ($status)
-        return 'OK';
+        echo 'file uploaded';
     else
-        return 'BLAD';
+        echo 'upps something went wrong';
 }
 
 
 if (count($_FILES)) {
     $status = upload_file($_FILES['upload'], 'application/pdf');
 }
-
-?>
+//
+//
+//?>
 <form action="?" method="post" enctype="multipart/form-data">
-    Wpisz ID umowy: <input name="contractId" value="" /><br/>
+    Wpisz ID umowy: <input name="contractId" value="contract name" /><br/>
     Tylko pliki PDF: <input type="file" name="upload" value=""/><br/>
     <input type="submit" name="send" value="upload" />
 </form>
