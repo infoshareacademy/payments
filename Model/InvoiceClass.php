b56def796f254f65a18264af517d51b03327b1c4
ceomarek <mrk.kolodziejczyk@gmail.com>
2015-10-16 15:14:50 +0200

    PAY-2
    - dzialajaca edycja oraz usuwanie rekordow
    - zamieszczenie drop down dla powiazanej tabeli Contracts

--- Model/InvoiceClass.php
+++ Model/InvoiceClass.php
@@ -9,13 +9,13 @@
 class InvoiceClass
 {
 
-    protected $id;
-    protected $signature_id;
-    protected $signature;
-    protected $amount;
-    protected $issue_date;
-    protected $maturity_date;
-    protected $payment_date;
+    public $id;
+    public $signature_id;
+    public $signature;
+    public $amount;
+    public $issue_date;
+    public $maturity_date;
+    public $payment_date;
 
     const MAX_LENGHT = 70;
     const SAVE_OK = 1;
@@ -33,16 +33,13 @@ class InvoiceClass
             $stmt = $this->pdo->query("select * from invoices where id=".(int)$id);
             if ($stmt->rowCount()>0) {
                 $result = $stmt->fetch(PDO::FETCH_ASSOC);
-
-                print_r($result);
-
                 $this->id = $result['id'];
-                $this->signature_id = $result['signature_id'];
-                $this->signature = $result['signature'];
-                $this->amount = $result['amount'];
-                $this->issue_date = $result['issue_date'];
-                $this->maturity_date = $result['maturity_date'];
-                $this->payment_date = $result['payment_date'];
+                $this->signature_id = $result['id_contract'];
+                $this->signature = $result['Signature'];
+                $this->amount = $result['Amount'];
+                $this->issue_date = $result['Issue_date'];
+                $this->maturity_date = $result['Maturity_date'];
+                $this->payment_date = $result['Payment_date'];
             }
             else {
                 throw new Exception('Brak takiego ID='.$id);
@@ -72,10 +69,10 @@ class InvoiceClass
                     $this->signature = htmlspecialchars($param_value);
                 break;
             case 'amount':
-                if (!$param_value || !(int)$param_value)
+                if (!$param_value || !(floatval($param_value)))
                     $this->amount = null;
                 else
-                    $this->amount = (int)$param_value;
+                    $this->amount = floatval($param_value);
                 break;
             case 'issue_date':
                 if (!$param_value || !preg_match('/^[\d]{4}\-[\d]{2}\-[\d]{2}$/', $param_value))
@@ -90,7 +87,7 @@ class InvoiceClass
                 $this->maturity_date = $param_value;
                 break;
             case 'payment_date':
-                if (!preg_match('/^[\d]{4}\-[\d]{2}\-[\d]{2}$/sx', $param_value))
+                if (!$param_value || !preg_match('/^[\d]{4}\-[\d]{2}\-[\d]{2}$/sx', $param_value))
                     $this->payment_date = null;
                 else
                 $this->payment_date = $param_value;
@@ -107,27 +104,41 @@ class InvoiceClass
     }
 
     public function save_to_db() {
+
+        print_r($this);
+
         if ($this->id) {
             $stmt = $this->pdo->prepare("UPDATE invoices SET
                 id=:identity,
-                id_contract=:signature_id,
-                Signature=:signature,
-                Amount=:amount,
-                Issue_date=:issue_date,
-                Maturity_date=:maturity_date,
-                Payment_date=:payment_date
-                WHERE id=:id
+                id_contract=:id_kontraktu,
+                Signature=:sygnatura,
+                Amount=:kwota,
+                Issue_date=:data_wystawienia,
+                Maturity_date=:data_platnosci,
+                Payment_date=:data_oplacenia
+                WHERE id=:identity
                 ");
+
+            print_r($stmt);
+
+            $input_parameters = array(
+                ':identity' => $this->id,
+                ':id_kontraktu' => $this->signature_id,
+                ':sygnatura' => $this->signature,
+                ':kwota' => $this->amount,
+                ':data_wystawienia' => $this->issue_date,
+                ':data_platnosci' => $this->maturity_date,
+                ':data_oplacenia' => $this->payment_date
+            );
+
+            print_r($input_parameters);
+
+            if($input_parameters[':data_oplacenia'] == null) {
+                $input_parameters['::data_oplacenia'] = 'NULL';
+            }
+
             $upload = $stmt->execute(
-                array(
-                    ':identity' => $this->id,
-                    ':signature_id' => $this->signature_id,
-                    ':signature' => $this->signature,
-                    ':amount' => $this->amount,
-                    ':issue_date' => $this->issue_date,
-                    ':maturity_date' => $this->maturity_date,
-                    ':payment_date' => $this->payment_date
-                )
+                $input_parameters
             );
         }
 
@@ -160,13 +171,20 @@ class InvoiceClass
             return self::SAVE_ERROR_DB;
 
     }
+
+// Metoda do pobierania rekordów z DB do wyświetlenia ich na stronie
+
+    public static function invoiceTable() {
+        $pdo = new PDO('mysql:dbname=infoshareaca_7;host=test.payments.infoshareaca.nazwa.pl', 'infoshareaca_7', 'F0r3v3r!');
+        $stmt = $pdo->query('SELECT * FROM invoices');
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
 }
 
 if (@$_GET['edit'] && (int)$_GET['edit']) {
     $edit = (int)$_GET['edit'];
     $invoice= new InvoiceClass($edit);
-
-    print_r($invoice);
 }
 
 if (@$_GET['delete'] && (int)$_GET['delete']) {
@@ -198,9 +216,10 @@ if (count($_POST)) {
         $error['maturity_date'] = 'Podaj poprawną datę lub skorzystaj z kalendarza';
 
     $invoice->payment_date = @$_POST['payment_date'];
-    if (!$invoice->payment_date)
-        $error['payment_date'] = 'Podaj poprawną datę lub skorzystaj z kalendarza';
+//    if (!$invoice->payment_date)
+//        $error['payment_date'] = 'Podaj poprawną datę lub skorzystaj z kalendarza';
 
+    print_r($error);
 
     if (!count($error)) {
         $upload = $invoice->save_to_db();
@@ -228,13 +247,43 @@ if (count($_POST)) {
 
 <?php
 
+    echo '<table>';
+        echo '<tr>';
+            echo '<th>ID</th>';
+            echo '<th>Numer umowy</th>';
+            echo '<th>Nazwa faktury</th>';
+            echo '<th>Data wystawienia</th>';
+            echo '<th>Data płatności</th>';
+            echo '<th>Data opłacenia</th>';
+        echo '</tr>';
+
+            $invoiceList = InvoiceClass::invoiceTable();
+            foreach ($invoiceList as $record) {
+
+                echo '<tr>';
+                echo '<td>'.$record['id'].'</td>';
+                echo '<td>'.$record['id_contract'].'</td>';
+                echo '<td>'.$record['Amount'].'</td>';
+                echo '<td>'.$record['Issue_date'].'</td>';
+                echo '<td>'.$record['Maturity_date'].'</td>';
+                echo '<td>'.$record['Payment_date'].'</td>';
+                echo '<td><a href="?edit='.$record['id'].'">edytuj</a> <a href="?delete='.$record['id'].'">usun</a></td>';
+                echo '</tr>';
+            }
+    echo '</table>';
+    echo '<br/><br/>';
+
     if (@$success)
         echo '<div style="color:#22aa22; font-weight:bold;">'.$success.'</div><br/>';
     if (@$error['general'])
         echo '<div style="color:#f00; font-weight:bold;">'.$error['general'].'</div><br/>';
 
 echo '<form action="?" method="post">';
+echo '<input name="id" type="hidden" value="'.@$invoice->id.'"/><br>';
 echo 'Numer umowy: <input name="signature_id" value="'.@$invoice->signature_id.'"/><br>';
+
+include_once('../select.php');
+
 echo 'Numer faktury: <input name="signature" value="'.@$invoice->signature.'"/><br><div style="color:#f00;">'.@$error['signature'].'</div>';
 echo 'Kwota: <input name="amount" value="'.@$invoice->amount.'" /><br><div style="color:#f00;">'.@$error['amount'].'</div>';
 echo 'Data wystawienia: <input name="issue_date" type="date" value="'.@$invoice->issue_date.'" /><br><div style="color:#f00;">'.@$error['issue_date'].'</div>';
@@ -245,22 +294,6 @@ echo '<button id="btn_send">'.(@$invoice->id ? 'ZACHOWAJ ZMIANY' : 'DODAJ NOWY')
 echo '</form>';
 
 ?>
-
-<br/><br/><br/>
-
-<a href="?">wyczysc formularz</a><br/>
-
-<br/><br/><br/>
-
-<a href="?edit=51">edytuj rekord 1</a><br/>
-<a href="?edit=52">edytuj rekord 2</a><br/>
-<a href="?edit=53">edytuj rekord 3</a><br/>
-
-<br/><br/>
-
-<a href="?delete=51">usun rekord 1</a><br/>
-<a href="?delete=52">usun rekord 2</a><br/>
-<a href="?delete=53">usun rekord 3</a><br/>
 
 </body>
 </html>
