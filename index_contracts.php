6a2e756475f6dc12899c4de78c4040ac4ebb0cfb
marek <mrk.kolodziejczyk@gmail.com>
2015-10-21 17:00:42 +0200

    Refactor kodu nr 4

--- index_contracts.php
+++ index_contracts.php
@@ -1,6 +1,6 @@
 <?php include 'src/partials/header.php'; ?>
 
 <h1 class="page-header">Contracts</h1>
-<?php include 'ContractsMainPage.php'; ?>
+<?php include 'src/partials/contractsMainPage.php'; ?>
 
 <?php include 'src/partials/footer.php'; ?>
