a4557cd524813ba65971fd53ec9bbc3ce7638f23
ceomarek <mrk.kolodziejczyk@gmail.com>
2015-10-30 14:56:39 +0100

    develop / poprawienie wyswietlania kalendarza

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 require_once 'src/functions/layout.php';
 require_once 'src/partials/invoiceForm.php';
-require_once 'src/partials/invoiceList.php';
+require_once 'src/partials/invoiceCalendar.php';
 
-echo layout('', invoiceForm() . invoiceList());
+ob_start();
+invoiceCalendar();
+$calendar = ob_get_clean();
+echo layout('', invoiceForm() . $calendar);
 
 
 
